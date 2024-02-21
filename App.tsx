f872a190c9b88d3fc1f62b887487d4c21a1dacac
GuilhermeDogini <guilhermedogini@hotmail.com>
2024-02-20 23:33:42 -0300

    basic-template

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 
+
 export default function App() {
 	return (
 		<View className='flex-1 items-center justify-center bg-green-500'>
